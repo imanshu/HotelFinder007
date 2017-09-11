dc1781a8543d9589d7a884f4762534e04c6651d6
imanshu <agarwal01anshu@gmail.com>
2017-09-11 16:35:25 +0530

    Update app.js
    commit1

--- app.js
+++ app.js
@@ -44,11 +44,12 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 /*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/*/
+.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 .onDefault((session) => {
     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
 */
+bot.recognizer(recognizer);
 bot.dialog('Greeting', function (session, args, next) {
 	session.send('welcome to the hotel finder ');
 
